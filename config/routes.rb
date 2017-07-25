c4aee52bd0ac64811b4df4cf50651ff67016e7fb
Brandon Cordell <brandon@brandoncordell.com>
2017-07-25 16:57:29 -0400

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     delete 'logout', to: 'devise/sessions#destroy'
-    get 'sign_up', to: 'devise/registrations#new'
+    get 'sign_up/:account_type', to: 'users/registrations#new'
   end
 
   namespace :admin do
