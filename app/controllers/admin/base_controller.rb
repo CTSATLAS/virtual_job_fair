dc6beaeddd089c48db9eb11cb492dd462b79567c
Brandon Cordell <brandon@brandoncordell.com>
2017-08-25 00:15:30 -0400

    Lock down admin to signed in users only

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,8 @@
 module Admin
   # Base controller for all admin namespaced controllers to inherit from
   class BaseController < ::ApplicationController
+    before_action :authenticate_user!
+
     layout 'admin'
   end
 end
