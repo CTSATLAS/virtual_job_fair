fe25fbc4ef38d2a5078dd048de4250a2f470c79e
Brandon Cordell <brandon@brandoncordell.com>
2017-08-24 23:32:33 -0400

    Redirect admin's to dashboard and users to root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    if resource.has_role? :admin
+      admin_root_path
+    else
+      root_path
+    end
+  end
 end
