a9bbf345a60726b8195d18ce984fdcaf2566ffb6
Brandon Cordell <brandon@brandoncordell.com>
2017-11-16 10:37:10 -0500

    Pull some data for the dashboard stats

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,12 @@ module Admin
   class DashboardController < BaseController
     authorize_resource class: false
 
-    def index; end
+    def index
+      @job_seeker_count = JobseekerProfile.count
+      @employer_count = EmployerProfile.count
+      @job_listing_count = JobListing.count
+      @latest_job_seekers = User.includes(:jobseeker_profile).with_role(:jobseeker).limit(5).order('id DESC')
+      @latest_employers = User.includes(:employer_profile).with_role(:employer).limit(5).order('id DESC')
+    end
   end
 end
